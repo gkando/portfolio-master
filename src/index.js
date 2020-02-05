1a4610603ae2f6ed69dea225d8706259575cb7a3
George Kando <gkando@gmail.com>
2020-02-05 14:29:55 -0600

    finished portfolio links

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './app.styles.scss';
-console.log('hi');
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
+
+document.querySelectorAll('div.portfolio-preview').forEach(function(element) {
+  element.addEventListener('click', function(e) {
+    console.log(e.target.dataset.src);
+    basicLightbox
+      .create(
+        `
+    <img width="1400" height="900" src=${e.target.dataset.src}>
+  `
+      )
+      .show();
+  });
+});
+
+// document.querySelector('a.portfolio-preview').onclick = () => {
+
+// };
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,74 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
 var path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: false,
+module.exports = env => {
+  return {
+    mode: env.MODE,
+    entry: './src/index.js',
+    devtool: env.MODE === 'development' ? 'inline-source-map' : 'none',
+    devServer: {
+      contentBase: './dist',
+    },
+    resolve: {
+      extensions: ['.js', 'scss', 'css'],
+    },
+    output: {
+      publicPath: '/',
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'index_bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              attrs: false,
+            },
           },
         },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jp(e*)g|svg)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                name: 'img/[hash]-[name].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyPlugin([
+        { from: 'node_modules/vanillajs-scrollspy/dist', to: 'vanillajs-scrollspy' },
+        { from: 'node_modules/basiclightbox/dist', to: 'basiclightbox' },
+        { from: 'src/img', to: 'img' },
+      ]),
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: 'public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+        chunkFilename: '[id].[hash].css',
+      }),
     ],
-  },
-
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin([
-      { from: 'node_modules/vanillajs-scrollspy/dist', to: 'vanillajs-scrollspy' },
-      { from: 'src/img', to: 'img' },
-    ]),
-    new HtmlWebpackPlugin({
-      inject: 'head',
-      template: 'public/index.html',
-    }),
-  ],
+  };
 };
 
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
